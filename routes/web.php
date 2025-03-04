3a3ac728e61b9649f803edc03e6b10e74dda34f2
Patrick Her <renfu.her@gmail.com>
2025-03-04 11:30:09 +0800

    移除學習記錄路由的註解程式碼
    - 清理 web.php 路由檔案中未使用的學習記錄路由
    - 刪除被註解的學習記錄相關路由設定

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,3 @@ Route::get('/backend', function () {
     return redirect('/backend/abouts');
 });
 
-// 學習記錄路由
-// Route::prefix('backend')->group(function () {
-//     // 學習記錄資源路由
-//     Route::resource('learning-records', LearningRecordController::class);
-
-//     // 會員學習記錄路由
-//     Route::get('members/{member}/learning-records', [LearningRecordController::class, 'memberRecords'])
-//         ->name('members.learning-records');
-// });
